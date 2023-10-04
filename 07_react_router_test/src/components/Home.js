e188077fc6932800f8bb0b4492d3745bfc9a55fe
Sara Arideh <sara.arzideh@gmail.com>
2023-10-04 21:16:22 +0300

    React Router Practice

---
+++ 07_react_router_test/src/components/Home.js
@@ -0,0 +1,6 @@
+const Home= (props)=>{
+    return(
+        <h2>Welcome to react router test</h2>
+    )
+}
+export default Home;
---
+++ 07_react_router_test/src/components/Secret.js
@@ -0,0 +1,6 @@
+const Secret=(props)=>{
+    return(
+        <h2>Welcome to the secret page</h2>
+    )
+}
+export default Secret;
